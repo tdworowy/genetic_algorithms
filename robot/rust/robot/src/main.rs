827c3d98c48d66fc4993120d02f76b60e44c4ae5
Tomasz Dworowy <dworowytomasz@gmail.com>
2023-01-22 11:26:06 +0100

    update

--- robot/rust/robot/src/main.rs
+++ robot/rust/robot/src/main.rs
@@ -26,7 +26,7 @@ enum State {
 enum Action {
     GoUp,
     GoDown,
-    GoRirht,
+    GoRight,
     GoLeft,
     TakePoint,
 }
@@ -35,11 +35,12 @@ fn get_random_action() -> Action {
     match rand::thread_rng().gen_range(0..=4) {
         0 => Action::GoUp,
         1 => Action::GoDown,
-        2 => Action::GoRirht,
+        2 => Action::GoRight,
         3 => Action::GoLeft,
         _ => Action::TakePoint,
     }
 }
+// TODO not all possible states are generated
 fn generate_strategy() -> HashMap<(State, State, State, State, State), Action> {
     /*0: above
     1: right
@@ -68,11 +69,105 @@ fn generate_strategy() -> HashMap<(State, State, State, State, State), Action> {
     });
     strategy
 }
+fn display_strategy(strategy: &HashMap<(State, State, State, State, State), Action>) {
+    for (k, v) in strategy {
+        println!("{:?} {:?}", k, v)
+    }
+}
+
 struct Robot {
     points: isize,
     grid: Vec<Vec<usize>>,
+    x: usize,
+    y: usize,
+}
+
+impl Robot {
+    fn new(grid: Vec<Vec<usize>>, x: Option<usize>, y: Option<usize>) -> Robot {
+        Robot {
+            points: 0,
+            grid,
+            x: x.unwrap_or(0),
+            y: y.unwrap_or(0),
+        }
+    }
+
+    fn get_state(&self) -> (State, State, State, State, State) {
+        let up: State = if self.y + 1 > self.grid.len() {
+            State::Wall
+        } else {
+            match self.grid[self.y + 1][self.x] {
+                1 => State::Point,
+                _ => State::Empty,
+            }
+        };
+
+        let down: State = if self.y as isize - 1 < 0 {
+            State::Wall
+        } else {
+            match self.grid[self.y + 1][self.x] {
+                1 => State::Point,
+                _ => State::Empty,
+            }
+        };
+
+        let right: State = if self.x + 1 > self.grid[0].len() {
+            State::Wall
+        } else {
+            match self.grid[self.y + 1][self.x] {
+                1 => State::Point,
+                _ => State::Empty,
+            }
+        };
+
+        let left: State = if self.x as isize - 1 < 0 {
+            State::Wall
+        } else {
+            match self.grid[self.y + 1][self.x] {
+                1 => State::Point,
+                _ => State::Empty,
+            }
+        };
+
+        let current = match self.grid[self.y][self.x] {
+            1 => State::Point,
+            _ => State::Empty,
+        };
+        (up, right, down, left, current)
+    }
+
+    fn play_strategy(
+        &mut self,
+        strategy: HashMap<(State, State, State, State, State), Action>,
+        steps: usize,
+    ) {
+        for _ in 0..steps {
+            let state = self.get_state();
+
+            println!("State {:?}", state);
+
+            let action = strategy.get(&state);
+            match action.unwrap() {
+                Action::GoUp => self.y += 1,
+                Action::GoDown => self.y -= 1,
+                Action::GoLeft => self.x -= 1,
+                Action::GoRight => self.x += 1,
+                Action::TakePoint => {
+                    self.points += 1;
+                    self.grid[self.x][self.y] = 0;
+                }
+            }
+        }
+    }
 }
 
 fn main() {
-    println!("{:?}", generate_strategy());
+    let grid = generate_gird_random(500, 500, vec![3, 7]);
+    let mut robot = Robot::new(grid, Some(0), Some(0));
+    let strategy = generate_strategy();
+
+    display_strategy(&strategy);
+    robot.play_strategy(strategy, 100);
+
+    println!("Points: {}", robot.points);
 }
